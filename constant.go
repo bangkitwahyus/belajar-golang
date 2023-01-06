293c54874761828adc86c386b80001da66825ef7
bangkitwahyus <bangkitwahyusaputra@gmail.com>
2023-01-06 13:44:28 +0700

    Menambahakan file constant.go

---
+++ constant.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Multiple Constant
+	const (
+		firstName string = "Roby"
+		lastName         = "Purba"
+		value            = 1000
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
