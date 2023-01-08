967e06dfeaffd9ab85c7d758c1d1cb4e53dfdb2c
bangkitwahyus <bangkitwahyusaputra@gmail.com>
2023-01-08 20:06:43 +0700

    Menambahkan file comparation-operator.go / Operasi Perbandingan

---
+++ comparation-operator.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Operasi Perbandingan
+
+	var name1 = "Eko"
+	var name2 = "Denny"
+
+	var result = name1 == name2
+
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
