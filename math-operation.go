c1d1c5460751978ab97db00b7ebdb7b84c71400a
bangkitwahyus <bangkitwahyusaputra@gmail.com>
2023-01-08 15:52:20 +0700

    Menambahkan file math-operation.go

---
+++ math-operation.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Operation Matematika
+
+	var hasil = 10 + 10
+	fmt.Println(hasil)
+
+	var a = 10
+	var b = 10
+	var c = a * b
+	fmt.Println(c)
+
+	//Augmented Assignment
+
+	var i = 10
+	i += 10 // i = i + 10
+	fmt.Println(i)
+
+	//Unary Operator
+
+	i++ // i = i + 1
+	fmt.Println(i)
+
+	var negatif = -100
+	var positif = +100
+
+	fmt.Println(negatif)
+	fmt.Println(positif)
+}
