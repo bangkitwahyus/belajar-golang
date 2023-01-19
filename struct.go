7fce1561b6200c40b9b601244ae66fbc1880094c
bangkitwahyus <bangkitwahyusaputra@gmail.com>
2023-01-19 15:27:05 +0700

    Menambahkan materi Struct Method

--- struct.go
+++ struct.go
@@ -7,6 +7,16 @@ type Customer struct {
 	Age           int
 }
 
+// Struct Method
+
+func (customer Customer) sayHi(name string) {
+	fmt.Println("Hello ", name, "My Name is ", customer.Name)
+}
+
+func (a Customer) sayHuuu() {
+	fmt.Println("Huuu from", a.Name)
+}
+
 func main() {
 
 	var doni Customer
@@ -14,19 +24,22 @@ func main() {
 	doni.Address = "Surabaya"
 	doni.Age = 27
 
-	fmt.Println(doni.Name)
-	fmt.Println(doni.Address)
-	fmt.Println(doni.Age)
+	doni.sayHi("Rio")
+	doni.sayHuuu()
+
+	// fmt.Println(doni.Name)
+	// fmt.Println(doni.Address)
+	// fmt.Println(doni.Age)
 
 	// Struct Literals
 
-	joko := Customer{
-		Name:    "Joko",
-		Address: "Jogjakarta",
-		Age:     25,
-	}
-	fmt.Println(joko)
+	// joko := Customer{
+	// 	Name:    "Joko",
+	// 	Address: "Jogjakarta",
+	// 	Age:     25,
+	// }
+	// fmt.Println(joko)
 
-	budi := Customer{"Budi", "Jakarta", 29}
-	fmt.Println(budi)
+	// budi := Customer{"Budi", "Jakarta", 29}
+	// fmt.Println(budi)
 }
